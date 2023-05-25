3ff158178f8f6c30f4b465c5786d0bb4044315c2
tekostyshyn <113828114+tekostyshyn@users.noreply.github.com>
2023-05-25 19:54:25 +0000

    Deploying to gh-pages from @ tekostyshyn/eliftech-delivery-app@0f8585099707656f85439153f4ae6c6d37290c56 🚀

--- static/js/137.843ba8e5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[137],{137:function(e,t,a){a.r(t);var r=a(184);t.default=function(){return(0,r.jsx)("main",{})}}}]);
-//# sourceMappingURL=137.843ba8e5.chunk.js.map
